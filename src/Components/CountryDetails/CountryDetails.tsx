1bde959af045e0bff98f18c8c3d8fad286edbee4
Mike Oram <Mikeoram01@gmail.com>
2022-12-15 19:32:02 +0000

    Addde some logic to getCountryBorders to check if it has no borders before running the query

--- src/Components/CountryDetails/CountryDetails.tsx
+++ src/Components/CountryDetails/CountryDetails.tsx
@@ -18,22 +18,25 @@ const CountryDetails = (): JSX.Element => {
         const countryData = await fetch(`https://restcountries.com/v3/name/${name}`)
         const countryDataJson = await countryData.json();
         setCountryData(countryDataJson[0]);
-        setBorderQueryString(countryDataJson[0].borders.join(','));
+        if (countryDataJson[0].borders !== undefined) {
+            setBorderQueryString(countryDataJson[0].borders.join(','));
+        }
     }
 
     const getBorderCountries = async () => {
-        const borderData = await fetch(`https://restcountries.com/v3/alpha?codes=${borderQueryString}`);
-        const borderDataJson = await borderData.json();
-        setBorderCountries(borderDataJson);
+        if (borderQueryString !== '') {
+            const borderData = await fetch(`https://restcountries.com/v3/alpha?codes=${borderQueryString}`);
+            const borderDataJson = await borderData.json();
+            setBorderCountries(borderDataJson);
+        }
     }
-    
+
     useEffect(() => {
         getCountryData();
     }, [name])
 
     useEffect(() => {
         if (Object.keys(countryData).length > 0) {
-            console.log(countryData)
             setCountryName(countryData.name.common)
             setCountryNativeName(Object.values(countryData.name.nativeName)[0].common);
             setCountryPopulation(countryData.population);
@@ -55,9 +58,7 @@ const CountryDetails = (): JSX.Element => {
             <p>Region: <span>{countryRegion}</span></p>
             <p>Capital: <span>{countryCapital}</span></p>
             <p>Top Level Domain: <span>{countryTLD}</span></p>
-            <p>Languages: <span>{countryLanguages.map((language) => {
-                return `${language} `
-            })}</span></p>
+            <p>Languages: <span>{countryLanguages.join(', ')}</span></p>
             {borderCountries.length > 0 ? borderCountries.map((country) => {
                 return <Link to={`/country/${country.name.common}`}>
                     {country.name.common}</Link>
